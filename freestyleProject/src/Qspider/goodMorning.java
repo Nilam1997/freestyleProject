11e04be489ac2f4498d888eff586b353457ebc16
Nilam1997 <118422638+Nilam1997@users.noreply.github.com>
2022-11-17 12:15:25 +0530

    Update goodMorning.java

--- freestyleProject/src/Qspider/goodMorning.java
+++ freestyleProject/src/Qspider/goodMorning.java
@@ -3,7 +3,7 @@ package Qspider;
 public class goodMorning 
 {public static void main(String[] args) 
 {
-	System.out.println("GOOD MORNING");
+	System.out.println("GOOD MORNING","how are you" );
 }
 
 }
